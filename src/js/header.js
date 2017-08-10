df68a10215e750ce83b5ef661687491ae74c4e2e
Raul Marty <santor3medio@gmail.com>
2017-08-10 17:14:59 -0500

    #12. Add window.location.pathname of solar-benefits.

--- src/js/header.js
+++ src/js/header.js
@@ -4,7 +4,7 @@ var ticking = false;
 var siteNavbar = document.querySelector('.site-navbar');
 //navbar-fixed-top
 function doSomething(scroll_pos) {
-  if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '/project-rer/index.html' || window.location.pathname === '/project-rer/') {
+  if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '/project-rer/index.html' || window.location.pathname === '/project-rer/' || window.location.pathname === '/project-rer/solar-benefits.html' || window.location.pathname === '/solar-benefits.html') {
     if (scroll_pos > 30) {
       siteNavbar.classList.add('nav-white-bg');
       siteNavbar.classList.remove('nav-transparent-bg');
