0428b4562c472429e3efbcf36b47c832d5156998
Raul Marty <santor3medio@gmail.com>
2017-08-16 16:39:35 -0500

    #29. Create carousel to view Proyectos.

---
+++ src/js/carousel-projects.js
@@ -0,0 +1,10 @@
+$('.project-image-carousel').slick({
+  dots: false,
+  infinite: false,
+  arrows: true,
+  speed: 300,
+  rows: 3,
+  slidesPerRow: 2,
+  nextArrow: '<div class="carousel-arrows rer-arrow-left"><img class="arrow-width-14px" src="img/icons/right-arrow.png"></div>',
+  prevArrow: '<div class="carousel-arrows rer-arrow-right"><img class="arrow-width-14px" src="img/icons/left-arrow.png"></div>'
+});
