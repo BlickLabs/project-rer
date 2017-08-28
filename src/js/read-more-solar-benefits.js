13c2d872028ded1d884058db7044f55b401ef923
Raul Marty <santor3medio@gmail.com>
2017-08-28 18:26:45 -0500

    #43. Create script to open text of icons in js file read-more-solar-benefits.

--- src/js/read-more-solar-benefits.js
+++ src/js/read-more-solar-benefits.js
@@ -1,8 +1,19 @@
-function readMore() {
-  var hideLayer = document.getElementById('why-layer-info');
-  if (hideLayer.style.display === 'none') {
-    hideLayer.style.display = 'block';
+function readMore(e) {
+  var mainInfoContainer = e.parentNode.parentNode.children[1],
+  	bodyInfo = mainInfoContainer.children[2];
+
+  if (window.getComputedStyle(bodyInfo, 'display').display === 'none') {
+  	bodyInfo.style.display = 'block';
   } else {
-    hideLayer.style.display = 'none';
+  	bodyInfo.style.display = 'none';
   }
+}
+
+function resetValues () {
+	document.querySelectorAll('.why-layer-info').forEach(function (layer) {
+		if (layer.id) {
+			layer.style.display = 'none';		
+		}
+	});
+
 }
