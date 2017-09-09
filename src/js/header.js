7f19aad88db9b8be91b660f62915e3f8d175892a
Raúl Marty <santor3medio@gmail.com>
2017-09-08 23:36:16 -0500

    #47. Fix touch in hamburger menu to js file header.

--- src/js/header.js
+++ src/js/header.js
@@ -33,19 +33,46 @@ window.addEventListener('scroll', function(e) {
   ticking = true;
 });
 
-window.addEventListener('click', function(e) {
-  if (!e.target.dataset.hasOwnProperty('attribute')) {
-    document.querySelector('#menuContainer').style.width = '0%';
-    document.querySelector('#hamburgerMenu').style.display = 'inline';
-  }
-});
+function showMenu() {
+  document.querySelector('#menuContainer').style.width = '85%';
+  document.querySelector('#hamburgerMenu').style.display = 'none';
+}
 
-function hideMenuHamburger () {
+function hideMenu() {
   document.querySelector('#menuContainer').style.width = '0%';
   document.querySelector('#hamburgerMenu').style.display = 'inline';
 }
 
-function showMenuHamburger () {
-  document.querySelector('#menuContainer').style.width = '85%';
-  document.querySelector('#hamburgerMenu').style.display = 'none';
-}
+window.addEventListener('click', function(e) {
+  if (!e.target.dataset.hasOwnProperty('attribute')) {
+    hideMenu();
+  }
+});
+
+/*Events of touchstart to mobile version*/
+
+var showHamburger = document.querySelector('#hamburgerMenu');
+
+showHamburger.addEventListener('touchstart', function(e) {
+  e.preventDefault();
+  showMenu();
+});
+
+var hideHamburger = document.querySelector('#hideHamburger');
+
+hideHamburger.addEventListener('touchstart', function(e) {
+  e.preventDefault();
+  hideMenu();
+});
+
+/* Events of click to desktop version*/
+
+showHamburger.addEventListener('click', function(e) {
+  e.preventDefault();
+  showMenu();
+});
+
+hideHamburger.addEventListener('click', function(e) {
+  e.preventDefault();
+  hideMenu();
+});
